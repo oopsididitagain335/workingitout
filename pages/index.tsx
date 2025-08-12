1878d5b5f5aa636e3480d2e7bc35e48bb25aa4dc
Solace <monkdev9@gmail.com>
2025-08-12 02:11:25 +0100

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 text-center px-4">
+      <h1 className="text-4xl md:text-6xl font-bold text-gray-800 mb-4">
+        Coming Soon
+      </h1>
+      <p className="text-lg md:text-xl text-gray-600">
+        We're currently in development. Stay tuned!
+      </p>
+    </div>
+  )
+}
+
+export default HomePage
