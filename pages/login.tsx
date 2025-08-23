ab1c4d612533dd0aca796c30a9bb2a057a2d12c5
Solace <monkdev9@gmail.com>
2025-08-24 00:27:21 +0100

    Update login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -8,7 +8,7 @@ export default function Login() {
   const [error, setError] = useState('');
   const router = useRouter();
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setError('');
 
