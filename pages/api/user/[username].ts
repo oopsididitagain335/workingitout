9a8f5072d4a0e1fccb3394a08a054f4e6dc4a76c
Solace <monkdev9@gmail.com>
2025-08-25 00:43:11 +0100

    Update [username].ts

--- pages/api/user/[username].ts
+++ pages/api/user/[username].ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import dbConnect from '../../../lib/dbConnect';
+import dbConnect from '../../../lib/db';
 import User from '../../../models/User';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
